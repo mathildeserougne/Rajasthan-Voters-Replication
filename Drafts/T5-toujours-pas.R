b6434157eed68d4f68b677b5aeba1d8c83c35756
msxrougne <mathilde.serougne@ensae.fr>
2025-06-17 15:12:48 +0000

    non!!!

--- Drafts/T5-toujours-pas.R
+++ Drafts/T5-toujours-pas.R
@@ -1,21 +1,32 @@
 ## REPLICATION - TABLE 5: KNOWLEDGE AND PERCEPTION OF THE NREGA ################
 ################################################################################
 
-# Installer et charger les packages nécessaires
-install.packages(c("haven", "dplyr", "tidyr", "fixest"))
-library(haven)
-library(dplyr)
-library(tidyr)
-library(fixest)
+# Installer les packages nécessaires
+install.packages(c("foreign","dplyr","tidyr","lmtest","sandwich","lfe","haven"))
 
-# Charger les données
+
+# Bibliothèques
+library(foreign) # Pour lire les fichiers .dta
+library(dplyr) # Pour la manipulation des données
+library(tidyr) # Pour le remodelage des données
+library(lmtest) # Pour les régressions
+library(sandwich) # Pour les erreurs standard robustes
+library(lfe) # Pour les effets fixes
+library(haven)
+
+# extraction des données en utilisant haven
 electoral_data <- read_dta("~/work/Electoral data cleaned.dta")
 household_data <- read_dta("~/work/Household survey data cleaned.dta")
 
-# Fusionner les données
+# preparation des donnees
+
+# fusion des données
 merged_data <- household_data %>%
   inner_join(electoral_data, by = c("district", "ps", "gp"))
 
+
+
+# moment où je dois reformatter des variables qui sont genrées...
 
 long_data <- merged_data %>%
   pivot_longer(
@@ -30,7 +41,7 @@ long_data <- merged_data %>%
     names_repair = "universal"
   )
 
-
+# nouvelles variables
 long_data <- long_data %>%
   mutate(
     C_I_AgeBelow25 = ifelse(A_age < 25, 1, 0),
@@ -57,6 +68,8 @@ long_data <- long_data %>%
 
 
 
+# Standardiser certaines variables
+# Standardiser certaines variables
 long_data <- long_data %>%
   group_by(`INT_treatment.x`, `RES05_gender.y`) %>%
   mutate(across(c(E_know_minimumwage_m, E_know_maximumdays_m, E_know_sarpanchrole_projects_m,
@@ -74,6 +87,7 @@ long_data <- long_data %>%
 
 
 
+# Création des variables d'index
 long_data <- long_data %>%
   mutate(
     E_know_nregarules = rowMeans(select(., starts_with("E_know_")), na.rm = TRUE),
@@ -86,14 +100,78 @@ long_data <- long_data %>%
   )
 
 
+# Exemple de création de TEMP_index et d'autres variables nécessaires
+long_data <- long_data %>%
+  mutate(
+    TEMP_index = index_empl_pre_svysample, # Assurez-vous que cette variable existe dans votre jeu de données
+    TEMP_X_res_index = RES05_gender.y * TEMP_index,
+    TEMP_X_anytr_index = `INT_treatment.x` * TEMP_index,
+    TEMP_X_grltr_index = `INT_treatment_general.x` * TEMP_index,
+    TEMP_X_gndtr_index = `INT_treatment_gender.x` * TEMP_index,
+    TEMP_X_anytr_res_index = `INT_treatment.x` * `RES05_gender.y` * TEMP_index,
+    TEMP_X_grltr_res_index = `INT_treatment_general.x` * `RES05_gender.y` * TEMP_index,
+    TEMP_X_gndtr_res_index = `INT_treatment_gender.x` * `RES05_gender.y` * TEMP_index
+  )
 
-# Exemple de régression avec effets fixes
-regression_results <- feols(E_know_nregarules ~ TEMP_X_res_index | ID_gp_no, data = long_data)
-summary(regression_results)
 
 
+library(lfe)
 
 
+### CA MARCHE PAS
+
+# Vérifiez les variables
+summary(long_data[, c("E_know_nregarules", "TEMP_X_res_index", "district", "ID_gp_no")])
+
+# Essayez une régression simple sans effets fixes
+simple_regression <- lm(E_know_nregarules ~ TEMP_X_res_index, data = long_data)
+summary(simple_regression)
+
+# Si la régression simple fonctionne, essayez d'ajouter les effets fixes
+regression_results <- try(felm(E_know_nregarules ~ TEMP_X_res_index | ID_gp_no, data = long_data))
+if (class(regression_results) == "try-error") {
+  print("Une erreur s'est produite avec les effets fixes.")
+} else {
+  summary(regression_results)
+}
+
+###
+
+## MARCHE PAS 
+
+# Vérifiez les valeurs manquantes
+summary(long_data[, c("E_know_nregarules", "TEMP_X_res_index", "ID_gp_no")])
+
+# Vérifiez la variabilité des variables
+table(long_data$ID_gp_no)
+
+# Essayez un sous-ensemble de données
+subset_data <- long_data[1:1000, ] # Utilisez un sous-ensemble de données
+regression_results <- try(felm(E_know_nregarules ~ TEMP_X_res_index | ID_gp_no, data = subset_data))
+if (class(regression_results) == "try-error") {
+  print("Une erreur s'est produite avec les effets fixes.")
+} else {
+  summary(regression_results)
+}
+
+####
 
 
+###
+
+# Vérifiez la colinéarité
+cor(subset_data[, c("E_know_nregarules", "TEMP_X_res_index")], use = "complete.obs")
+
+# Essayez un modèle simplifié sans effets fixes
+simple_regression <- lm(E_know_nregarules ~ TEMP_X_res_index, data = subset_data)
+summary(simple_regression)
+
+# Si le modèle simplifié fonctionne, essayez d'ajouter les effets fixes avec un sous-ensemble plus petit
+small_subset_data <- subset_data[1:500, ] # Utilisez un sous-ensemble plus petit
+regression_results <- try(felm(E_know_nregarules ~ TEMP_X_res_index | ID_gp_no, data = small_subset_data))
+if (class(regression_results) == "try-error") {
+  print("Une erreur s'est produite avec les effets fixes.")
+} else {
+  summary(regression_results)
+}
 
