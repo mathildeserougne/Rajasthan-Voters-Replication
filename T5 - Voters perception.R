e2c32f7d25d004440a1eb002cbe97baba6976830
msxrougne <mathilde.serougne@ensae.fr>
2025-06-17 11:20:55 +0000

    pfff

--- T5 - Voters perception.R
+++ T5 - Voters perception.R
@@ -2,134 +2,202 @@
 ################################################################################
 
 # Installer les packages nécessaires
-install.packages(c("dplyr", "tidyr", "fixest", "stargazer", "haven"))
+install.packages(c("foreign","dplyr","tidyr","lmtest","sandwich","lfe","haven"))
 
-# Charger les bibliothèques
-library(dplyr)
-library(tidyr)
-library(fixest)
-library(stargazer)
+
+# Bibliothèques
+library(foreign) # Pour lire les fichiers .dta
+library(dplyr) # Pour la manipulation des données
+library(tidyr) # Pour le remodelage des données
+library(lmtest) # Pour les régressions
+library(sandwich) # Pour les erreurs standard robustes
+library(lfe) # Pour les effets fixes
 library(haven)
 
-# Charger les données
+# extraction des données en utilisant haven
 electoral_data <- read_dta("~/work/Electoral data cleaned.dta")
 household_data <- read_dta("~/work/Household survey data cleaned.dta")
 
+# preparation des donnees
 
-
-
-
-
-
-
-
-# Nettoyer les données électorales
-electoral_data_clean <- electoral_data %>%
-  select(district, ps, gp, GP_population, GP_lit, GP_sc, GP_st, GP_nbvillages,
-         RES00_gender, RES00_obc, RES00_sc, RES00_st, RES10_obc, RES10_sc, RES10_st,
-         RES05_obc, RES05_sc, RES05_st, index_empl_pre_svysample, starts_with("std_HH_NREGA"))
-
-# Fusionner les données
+# fusion des données
 merged_data <- household_data %>%
-  inner_join(electoral_data_clean, by = c("district", "ps", "gp")) %>%
-  filter(!duplicated(paste(district, ps, gp)))
-
-# Vérifier les données fusionnées
-head(merged_data)
-
-# relationship normale on va dire
-merged_data <- household_data %>%
-  inner_join(electoral_data_clean, by = c("district", "ps", "gp"), relationship = "many-to-many") %>%
-  filter(!duplicated(paste(district, ps, gp)))
+  inner_join(electoral_data, by = c("district", "ps", "gp"))
 
 
 
+# moment où je dois reformatter des variables qui sont genrées...
 
-## individual transformation
+long_data <- merged_data %>%
+  pivot_longer(
+    cols = c(A_age_m, A_age_f, A_literacy_m, A_literacy_f, A_educ_m, A_educ_f, D_NREGA_work_m, D_NREGA_work_f),
+    names_to = c("variable", "gender"),
+    names_pattern = "(.*)_(.)",
+    values_to = "value"
+  ) %>%
+  pivot_wider(
+    names_from = variable,
+    values_from = value,
+    names_repair = "universal"
+  )
 
-# Sélectionner les colonnes nécessaires
-individual_data <- merged_data %>%
-  select(district, ps, gp, ID_gp_no, starts_with("A_"), starts_with("D_"), starts_with("E_"),
-         starts_with("F_"), starts_with("H_"), starts_with("GP_"), starts_with("ELEC10_"),
-         starts_with("RES"), starts_with("INT_"), starts_with("X_"), starts_with("index_"),
-         starts_with("std_HH_NREGA"))
-
-# Ajouter un identifiant temporaire
-individual_data <- individual_data %>%
-  mutate(TEMP_id = row_number())
-
-# Transformer les données en format long
-individual_data_long <- individual_data %>%
-  pivot_longer(cols = c(starts_with("A_"), starts_with("D_"), starts_with("E_"), starts_with("F_")),
-               names_to = "variable", values_to = "value") %>%
-  separate(variable, into = c("prefix", "suffix"), sep = "_", remove = FALSE) %>%
-  pivot_wider(names_from = suffix, values_from = value)
-
-# Vérifier les données transformées
-head(individual_data_long)
-
-
-
-
-
-## composite variables
-
-# Créer les variables composites
-individual_data_long <- individual_data_long %>%
+# nouvelles variables
+long_data <- long_data %>%
   mutate(
-    E_know_nregarules = rowMeans(select(., starts_with("E_know_minimumwage"), starts_with("E_know_maximumdays")), na.rm = TRUE),
-    E_know_sarpanchrole = rowMeans(select(., starts_with("E_know_sarpanchrole_")), na.rm = TRUE),
-    E_rate_nrega = rowMeans(select(., starts_with("E_rate_NREGAimplementation"), starts_with("E_rate_sarpanchperformance")), na.rm = TRUE),
-    F_rate_publicgoods = rowMeans(select(., starts_with("F_rate_publicgoods_road"), starts_with("F_rate_publicgoods_pump"), starts_with("F_rate_publicgoods_school")), na.rm = TRUE)
+    C_I_AgeBelow25 = ifelse(A_age < 25, 1, 0),
+    C_I_Age2535 = ifelse(A_age >= 25 & A_age < 35, 1, 0),
+    C_I_Age3545 = ifelse(A_age >= 35 & A_age < 45, 1, 0),
+    C_I_AgeAbove45 = ifelse(A_age >= 45 & !is.na(A_age), 1, 0),
+    C_I_Female = ifelse(gender == "f", 1, 0),
+    C_I_Literate = ifelse(A_literacy == 4, 1, 0),
+    C_I_EducNone = ifelse(A_educ == 0, 1, 0),
+    C_I_EducPrimary = ifelse(A_educ > 0 & A_educ <= 5, 1, 0),
+    C_I_EducLowerSec = ifelse(A_educ > 5 & A_educ <= 9, 1, 0),
+    C_I_EducUpperSec = ifelse(A_educ > 9 & A_educ <= 12, 1, 0),
+    C_I_EducTertiary = ifelse(A_educ > 12 & !is.na(A_educ), 1, 0),
+    C_I_Missing = ifelse(is.na(A_age) | is.na(A_educ) | is.na(A_literacy), 1, 0),
+    C_H_bpl = ifelse(H_bpl == 1, 1, 0),
+    C_H_ownland = ifelse(H_ownland == 1, 1, 0),
+    C_H_hindu = ifelse(H_religion == 1, 1, 0),
+    C_H_CasteGen = ifelse(H_caste == 1 | H_caste == 5, 1, 0),
+    C_H_CasteOBC = ifelse(H_caste == 2 | H_caste == 6, 1, 0),
+    C_H_CasteSC = ifelse(H_caste == 3, 1, 0),
+    C_H_CasteST = ifelse(H_caste == 4, 1, 0),
+    C_H_Missing = ifelse(is.na(H_bpl) | is.na(H_ownland) | is.na(H_religion) | is.na(H_caste), 1, 0)
   )
 
-# Vérifier les variables composites
-head(individual_data_long)
 
 
 
 
 
-## doing the regressions
 
-# Définir les variables de contrôle
-gpcontrols <- c("GP_population", "GP_lit", "GP_sc", "GP_st", "GP_nbvillages", "RES00_gender", "RES00_obc", "RES00_sc", "RES00_st", "RES10_obc", "RES10_sc", "RES10_st", "RES05_obc", "RES05_sc", "RES05_st")
-indcontrols <- grep("C_I_", names(individual_data_long), value = TRUE)
-hhcontrols <- grep("C_H_", names(individual_data_long), value = TRUE)
 
-# Définir les variables dépendantes
-dep_vars <- c("E_know_nregarules", "E_know_sarpanchrole", "E_rate_nrega", "F_rate_publicgoods")
+## REPLICATION - TABLE 5: KNOWLEDGE AND PERCEPTION OF THE NREGA ################
+################################################################################
 
-# Exécuter les régressions
-regression_results <- list()
-for (dep_var in dep_vars) {
-  # Calculer les moyennes de contrôle
-  control_mean1 <- individual_data_long %>%
-    filter(INT_treatment == 0 & RES05_gender == 0) %>%
-    summarise(mean = mean(!!sym(dep_var), na.rm = TRUE)) %>%
-    pull(mean)
-  
-  control_mean2 <- individual_data_long %>%
-    filter(INT_treatment == 0 & RES05_gender == 1) %>%
-    summarise(mean = mean(!!sym(dep_var), na.rm = TRUE)) %>%
-    pull(mean)
-  
-  # Exécuter la régression
-  regression <- feols(
-    reformulate(termlabels = c("index_empl_pre_svysample", gpcontrols, indcontrols, hhcontrols, "district"), response = dep_var),
-    data = individual_data_long,
-    cluster = ~ID_gp_no
+# Installer les packages nécessaires
+install.packages(c("foreign","dplyr","tidyr","lmtest","sandwich","lfe","haven"))
+
+
+# Bibliothèques
+library(foreign) # Pour lire les fichiers .dta
+library(dplyr) # Pour la manipulation des données
+library(tidyr) # Pour le remodelage des données
+library(lmtest) # Pour les régressions
+library(sandwich) # Pour les erreurs standard robustes
+library(lfe) # Pour les effets fixes
+library(haven)
+
+# extraction des données en utilisant haven
+electoral_data <- read_dta("~/work/Electoral data cleaned.dta")
+household_data <- read_dta("~/work/Household survey data cleaned.dta")
+
+# preparation des donnees
+
+# fusion des données
+merged_data <- household_data %>%
+  inner_join(electoral_data, by = c("district", "ps", "gp"))
+
+
+
+# moment où je dois reformatter des variables qui sont genrées...
+
+long_data <- merged_data %>%
+  pivot_longer(
+    cols = c(A_age_m, A_age_f, A_literacy_m, A_literacy_f, A_educ_m, A_educ_f, D_NREGA_work_m, D_NREGA_work_f),
+    names_to = c("variable", "gender"),
+    names_pattern = "(.*)_(.)",
+    values_to = "value"
+  ) %>%
+  pivot_wider(
+    names_from = variable,
+    values_from = value,
+    names_repair = "universal"
   )
-  
-  # Stocker les résultats
-  regression_results[[dep_var]] <- list(
-    regression = regression,
-    control_mean1 = control_mean1,
-    control_mean2 = control_mean2
-  )
-}
 
-# Vérifier les résultats des régressions
-str(regression_results)
+# nouvelles variables
+long_data <- long_data %>%
+  mutate(
+    C_I_AgeBelow25 = ifelse(A_age < 25, 1, 0),
+    C_I_Age2535 = ifelse(A_age >= 25 & A_age < 35, 1, 0),
+    C_I_Age3545 = ifelse(A_age >= 35 & A_age < 45, 1, 0),
+    C_I_AgeAbove45 = ifelse(A_age >= 45 & !is.na(A_age), 1, 0),
+    C_I_Female = ifelse(gender == "f", 1, 0),
+    C_I_Literate = ifelse(A_literacy == 4, 1, 0),
+    C_I_EducNone = ifelse(A_educ == 0, 1, 0),
+    C_I_EducPrimary = ifelse(A_educ > 0 & A_educ <= 5, 1, 0),
+    C_I_EducLowerSec = ifelse(A_educ > 5 & A_educ <= 9, 1, 0),
+    C_I_EducUpperSec = ifelse(A_educ > 9 & A_educ <= 12, 1, 0),
+    C_I_EducTertiary = ifelse(A_educ > 12 & !is.na(A_educ), 1, 0),
+    C_I_Missing = ifelse(is.na(A_age) | is.na(A_educ) | is.na(A_literacy), 1, 0),
+    C_H_bpl = ifelse(H_bpl == 1, 1, 0),
+    C_H_ownland = ifelse(H_ownland == 1, 1, 0),
+    C_H_hindu = ifelse(H_religion == 1, 1, 0),
+    C_H_CasteGen = ifelse(H_caste == 1 | H_caste == 5, 1, 0),
+    C_H_CasteOBC = ifelse(H_caste == 2 | H_caste == 6, 1, 0),
+    C_H_CasteSC = ifelse(H_caste == 3, 1, 0),
+    C_H_CasteST = ifelse(H_caste == 4, 1, 0),
+    C_H_Missing = ifelse(is.na(H_bpl) | is.na(H_ownland) | is.na(H_religion) | is.na(H_caste), 1, 0)
+  )
+
+
+
+# Standardiser certaines variables
+# Standardiser certaines variables
+long_data <- long_data %>%
+  group_by(`INT_treatment.x`, `RES05_gender.y`) %>%
+  mutate(across(c(E_know_minimumwage_m, E_know_maximumdays_m, E_know_sarpanchrole_projects_m,
+                  E_know_sarpanchrole_jobcard_m, E_know_sarpanchrole_work_m, E_know_jobcardapplication_m,
+                  E_know_waitingdays_m, E_know_unemploymentallowance_m, E_know_postofficepay_m,
+                  E_rate_NREGAimplementation_m, E_rate_NREGAimplementation_g_m, E_rate_NREGAimplementation_vg_m,
+                  E_rate_sarpanchperformance_m, E_rate_sarpanchperformance_g_m, E_rate_sarpanchperformance_vg_m,
+                  E_know_minimumwage_f, E_know_maximumdays_f, E_know_sarpanchrole_projects_f,
+                  E_know_sarpanchrole_jobcard_f, E_know_sarpanchrole_work_f, E_know_jobcardapplication_f,
+                  E_know_waitingdays_f, E_know_unemploymentallowance_f, E_know_postofficepay_f,
+                  E_rate_NREGAimplementation_f, E_rate_NREGAimplementation_g_f, E_rate_NREGAimplementation_vg_f,
+                  E_rate_sarpanchperformance_f, E_rate_sarpanchperformance_g_f, E_rate_sarpanchperformance_vg_f),
+                ~ ifelse(`INT_treatment.x` == 0 & `RES05_gender.y` == 0, scale(.x), .x))) %>%
+  ungroup()
+
+
+
+# Création des variables d'index
+long_data <- long_data %>%
+  mutate(
+    E_know_nregarules = rowMeans(select(., starts_with("E_know_")), na.rm = TRUE),
+    E_know_sarpanchrole = rowMeans(select(., E_know_sarpanchrole_projects_m, E_know_sarpanchrole_jobcard_m, E_know_sarpanchrole_work_m,
+                                          E_know_sarpanchrole_projects_f, E_know_sarpanchrole_jobcard_f, E_know_sarpanchrole_work_f), na.rm = TRUE),
+    E_rate_nrega = rowMeans(select(., E_rate_NREGAimplementation_m, E_rate_sarpanchperformance_m,
+                                   E_rate_NREGAimplementation_f, E_rate_sarpanchperformance_f), na.rm = TRUE),
+    F_rate_publicgoods = rowMeans(select(., F_rate_publicgoods_road_m, F_rate_publicgoods_pump_m, F_rate_publicgoods_school_m,
+                                         F_rate_publicgoods_road_f, F_rate_publicgoods_pump_f, F_rate_publicgoods_school_f), na.rm = TRUE)
+  )
+
+
+# Exemple de création de TEMP_index et d'autres variables nécessaires
+long_data <- long_data %>%
+  mutate(
+    TEMP_index = index_empl_pre_svysample, # Assurez-vous que cette variable existe dans votre jeu de données
+    TEMP_X_res_index = RES05_gender.y * TEMP_index,
+    TEMP_X_anytr_index = `INT_treatment.x` * TEMP_index,
+    TEMP_X_grltr_index = `INT_treatment_general.x` * TEMP_index,
+    TEMP_X_gndtr_index = `INT_treatment_gender.x` * TEMP_index,
+    TEMP_X_anytr_res_index = `INT_treatment.x` * `RES05_gender.y` * TEMP_index,
+    TEMP_X_grltr_res_index = `INT_treatment_general.x` * `RES05_gender.y` * TEMP_index,
+    TEMP_X_gndtr_res_index = `INT_treatment_gender.x` * `RES05_gender.y` * TEMP_index
+  )
+
+
+
+library(lfe)
+
+# Exemple de régression avec effets fixes
+regression_results <- felm(E_know_nregarules ~ TEMP_X_res_index + factor(district) | ID_gp_no, data = long_data)
+summary(regression_results)
+
+
+
+
 
 
