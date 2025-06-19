a87940f1a587553481aa1b854150365487299a7e
msxrougne <mathilde.serougne@ensae.fr>
2025-06-19 11:03:46 +0000

    jusqu'ici ça marche pitier

--- T5 - Voters perception.R
+++ T5 - Voters perception.R
@@ -47,12 +47,10 @@ outregvarindex2 <- paste(TEMP_X_anytr_index, TEMP_X_anytr_res_index, TEMP_X_res_
 outregvarindex3 <- paste(TEMP_X_gender_index, TEMP_X_general_index, TEMP_X_gender_res_index, TEMP_X_general_res_index, TEMP_X_res_index, outregvar4, TEMP_index)
 
 
-### DATA 
 
-# Charger les bibliothèques nécessaires
-library(haven)
-library(dplyr)
-library(tidyr)
+
+## version du code du 19/06
+
 
 # Chemins des fichiers
 electoral_data_path <- "~/work/Electoral data cleaned.dta"
@@ -93,67 +91,6 @@ household_data <- household_data %>%
 merged_data <- household_data %>%
   inner_join(read_dta(temp_file), by = c("district", "ps", "gp"))
 
-
-
-
-#Transformation des données de wide à long
-merged_data_long <- merged_data %>%
-  mutate(TEMP_id = row_number()) %>%
-  pivot_longer(
-    cols = c(
-      starts_with("A_age"), starts_with("A_educ"), starts_with("A_literacy"),
-      D_NREGA_work_m, D_NREGA_work_f, starts_with("E_know"),
-      starts_with("E_rate"), starts_with("F_rank"),
-      starts_with("F_rate_publicgoods"), starts_with("F_optimistic")
-    ),
-    names_to = c("variable", "gender"),
-    values_to = "value",
-    names_pattern = "([^_]+)_([^_]+)"
-  ) %>%
-  mutate(value = ifelse(variable == "A_age" & value == 0, NA, value))
-
-# Afficher les premières lignes du jeu de données transformé
-head(merged_data_long)
-
-colnames(performance_data)
-colnames(electoral_data)
-colnames(household_data)
-colnames(merged_data)
-
-colnames(merged_data_long)
-
-
-
-
-## new variables
-
-merged_data_long <- merged_data_long %>%
-  mutate(
-    C_I_AgeBelow25 = ifelse(A_age < 25, 1, 0),
-    C_I_Age2535 = ifelse(A_age >= 25 & A_age < 35, 1, 0),
-    C_I_Age3545 = ifelse(A_age >= 35 & A_age < 45, 1, 0),
-    C_I_AgeAbove45 = ifelse(A_age >= 45 & !is.na(A_age), 1, 0),
-    C_I_Female = ifelse(gender == "_f", 1, 0),
-    C_I_Literate = ifelse(A_literacy == 4, 1, 0),
-    C_I_EducNone = ifelse(A_educ == 0, 1, 0),
-    C_I_EducPrimary = ifelse(A_educ > 0 & A_educ <= 5, 1, 0),
-    C_I_EducLowerSec = ifelse(A_educ > 5 & A_educ <= 9, 1, 0),
-    C_I_EducUpperSec = ifelse(A_educ > 9 & A_educ <= 12, 1, 0),
-    C_I_EducTertiary = ifelse(A_educ > 12 & !is.na(A_educ), 1, 0),
-    C_I_Missing = as.integer(is.na(A_age) | is.na(A_educ) | is.na(A_literacy)),
-    C_H_bpl = ifelse(H_bpl == 1, 1, 0),
-    C_H_ownland = ifelse(H_ownland == 1, 1, 0),
-    C_H_hindu = ifelse(H_religion == 1, 1, 0),
-    C_H_CasteGen = ifelse(H_caste == 1 | H_caste == 5, 1, 0),
-    C_H_CasteOBC = ifelse(H_caste == 2 | H_caste == 6, 1, 0),
-    C_H_CasteSC = ifelse(H_caste == 3, 1, 0),
-    C_H_CasteST = ifelse(H_caste == 4, 1, 0),
-    C_H_Missing = as.integer(is.na(H_bpl) | is.na(H_ownland) | is.na(H_religion) | is.na(H_caste))
-  )
-
-
-## marche pas donc autre essai
-
 # Transformation des données de wide à long
 merged_data_long <- merged_data %>%
   mutate(TEMP_id = row_number()) %>%
@@ -164,15 +101,14 @@ merged_data_long <- merged_data %>%
       starts_with("E_rate"), starts_with("F_rank"),
       starts_with("F_rate_publicgoods"), starts_with("F_optimistic")
     ),
-    names_to = "variable",
+    names_to = c("var_prefix", "gender"),
     values_to = "value",
-    names_pattern = "([^_]+)_(.*)"
+    names_pattern = "([^_]+)_([^_]+)"
   ) %>%
   mutate(
-    A_age = ifelse(variable == "A_age", value, NA),
-    gender = ifelse(variable == "gender", as.character(value), NA),
-    A_literacy = ifelse(variable == "A_literacy", value, NA),
-    A_educ = ifelse(variable == "A_educ", value, NA)
+    A_age = as.numeric(ifelse(var_prefix == "A_age", value, NA)),
+    A_educ = as.numeric(ifelse(var_prefix == "A_educ", value, NA)),
+    A_literacy = as.numeric(ifelse(var_prefix == "A_literacy", value, NA))
   ) %>%
   group_by(TEMP_id) %>%
   mutate(
@@ -180,7 +116,7 @@ merged_data_long <- merged_data %>%
     C_I_Age2535 = ifelse(A_age >= 25 & A_age < 35, 1, 0),
     C_I_Age3545 = ifelse(A_age >= 35 & A_age < 45, 1, 0),
     C_I_AgeAbove45 = ifelse(A_age >= 45 & !is.na(A_age), 1, 0),
-    C_I_Female = ifelse(gender == "_f", 1, 0),
+    C_I_Female = ifelse(gender == "f", 1, 0),
     C_I_Literate = ifelse(A_literacy == 4, 1, 0),
     C_I_EducNone = ifelse(A_educ == 0, 1, 0),
     C_I_EducPrimary = ifelse(A_educ > 0 & A_educ <= 5, 1, 0),
@@ -196,7 +132,24 @@ merged_data_long <- merged_data %>%
     C_H_CasteSC = ifelse(H_caste == 3, 1, 0),
     C_H_CasteST = ifelse(H_caste == 4, 1, 0),
     C_H_Missing = as.integer(is.na(H_bpl) | is.na(H_ownland) | is.na(H_religion) | is.na(H_caste))
-  )
+  ) %>%
+  ungroup()
+
+# Afficher les premières lignes du jeu de données transformé
+head(merged_data_long)
+
+
+# Afficher les premières lignes du jeu de données transformé
+head(merged_data_long)
+
+
+
+
+
+
+
+
+
 
 
 
